3c36a1383fb7d79dc2752f1b14e4ec7f4a8229fb
mosijituo049 <zhiwen0921@gmail.com>
2026-07-01 13:11:01 +0200

    build streamlit app

---
+++ app/Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="GA4 Product Analytics",
+    page_icon="📊",
+    layout="wide",
+)
+
+st.title("📊 GA4 Product Analytics")
+
+st.write("Welcome!")
+
+st.success("Streamlit is running.")
+
+if __name__ == "__main__":
+    print("Home Page")
---
+++ app/pages/01_Funnel.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("📈 Funnel Analysis")
+
+st.write("Hello Funnel")
